6394802d3f1445b7f8637f39ffad8d3ff5ec7232
Kamile <kamile.kaydee@gmail.com>
2020-09-05 16:38:01 -0400

    Finished Chapter 3: Stacks

--- 3_Stacks_Queues.py
+++ 3_Stacks_Queues.py
@@ -23,9 +23,14 @@ class Stack:
         if len(self.stack) == 0:
             return
         else:
-            self.stack.pop(len(self.stack)-1)
-            self.top = self.stack[len(self.stack)-1]
-            self.topIndex = len(self.stack)-1
+            popped = self.stack.pop(len(self.stack)-1)
+            if len(self.stack)>0:
+                self.top = self.stack[len(self.stack)-1]
+                self.topIndex = len(self.stack)-1
+            else:
+                self.top = None
+                self.topIndex = 0
+            return popped
 
     def peek(self):
         #return top, don't remove
@@ -121,6 +126,8 @@ class SetOfStacks():
         else:
             self.stack[tempTop] = None
 
+
+
     def printStack(self):
         printList = []
         index = 0
@@ -161,11 +168,161 @@ class MyQueue:
 # Q5: Write a program to sort stack such that the smallest items are on top. Use an additional temporary stack, do not copy elements into other data structures (such as array)
 # Stack supports push, pop, peek and isEmpty
 def question3_5():
-    print("----------\n" + "3.5 Write a program to sort stack such that the smallest items are on top. Use an additional temporary stack, do not copy elements into other data structures (such as array) Stack supports push, pop, peek and isEmpty\n" + "Input : ", end='')
+    print("----------\n" + "3.5 Write a program to sort stack such that the smallest items are on top. Use an additional temporary stack, do not copy elements into other data structures (such as array) Stack supports push, pop, peek and isEmpty\n")
+
+class sortStack():
+    def __init__(self,array):
+        self.stack = array
+        self.top = self.stack[len(self.stack)-1]
+
+    def sortPush(self, new:int):
+        if len(self.stack) == 0:
+            self.stack.push(new)
+        else:
+            temp = Stack([0])
+            temp.pop()
+            while self.sortPeek() < new:
+                item = self.stack.pop()
+                temp.push(item)
+            # temp.printStack()
+            self.stack.append(new)
+            self.top = self.stack[len(self.stack)-1]
+            # self.printSortStack()
+            while len(temp.stack) > 0:
+                item = temp.pop()
+                self.stack.append(item)
+        self.top = self.stack[len(self.stack)-1]
+
+    def sortPop(self):
+        self.stack.pop()
+
+    def sortPeek(self):
+        #return top, don't remove
+          #remove the top
+        if len(self.stack) == 0:
+            return None
+        elif len(self.stack) == 1:
+            return self.stack[0]
+        else:
+            return self.stack[len(self.stack)-1]
+
+    def printSortStack(self):
+        printList = []
+        index = 0
+        item = self.stack[index]
+        while(item != self.top):
+            printList.insert(0, item)
+            index = index + 1
+            item = self.stack[index]
+        printList.insert(0, item)
+        print(printList)
+
 
 # Q6: Animal Shelter holds only dogs & cats, operates in 'first in, first out'. People must adopt oldest (based on time of arrival) of dogs, or can select if they want dog or cat (and get oldest animal of that type). Cannot select which specific animal they want. Create a data structure that maintains this system. Implement Enquque, deququeAny, dequeueDog, dequeueCat. You can use a LinkedList structure.
 def question3_6():
     print("----------\n" + "3.6 Animal Shelter holds only dogs & cats, operates in 'first in, first out'. People must adopt oldest (based on time of arrival) of dogs, or can select if they want dog or cat (and get oldest animal of that type). Cannot select which specific animal they want. Create a data structure that maintains this system. Implement Enquque, deququeAny, dequeueDog, dequeueCat. You can use a LinkedList structure.\n")
+    # My LinkedList Library, edited specifically for the Animal Shelter
+class Node():
+  def __init__(self, value, type, n_next, n_prev):
+    self.value = value #name
+    self.type = type #dog or cat
+    self.n_next = n_next
+    self.n_prev = n_prev
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = self.head	# head and tail are the same at first
+        self.ptr = self.head
+
+    def enqueue(self, name, type):
+        newNode = Node(name, type, None, None)
+        #if empty, add as head:
+        if self.head == None:
+          self.head = newNode
+          self.tail = newNode
+          self.ptr = newNode
+        #else, add into index
+        else:
+            while(self.ptr.n_next):
+              self.next()
+            #now at end of linkedlist
+            prevNode = self.ptr
+            newNode.n_next = None
+            newNode.n_prev = prevNode
+            prevNode.n_next = newNode
+        self.ptr = self.head
+
+
+    def print_linked_list(self):
+        ptr = self.head
+        print('[ ', end = '')
+        while(ptr):
+          print('Name:', ptr.value, ',', 'Type:', ptr.type, end = ' | ')
+          ptr = ptr.n_next
+        print(']')
+
+    def get_size(self):
+        size = 0
+        ptr = self.head
+        while(ptr):
+          size = size + 1
+          ptr = ptr.n_next
+        return size
+
+    def prev(self):
+        if self.ptr.n_prev:
+            self.ptr = self.ptr.n_prev
+            return self.ptr.value
+
+    def next(self):
+        if self.ptr.n_next:
+            self.ptr = self.ptr.n_next
+            return self.ptr.value
+        else:
+            self.ptr = None
+            return None
+
+    def dequeue(self):
+        #if current pointer exists
+        if self.ptr:
+            #if this is head
+            if self.ptr == self.head:
+                self.ptr.n_next.n_prev = None
+                self.head = self.ptr.n_next
+            #if this is tail
+            elif self.ptr.n_next == None:
+                self.ptr.n_prev.n_next = None
+                self.tail = self.ptr.n_prev
+            #if this is not head or tail
+            else:
+                self.ptr.n_prev.n_next = self.ptr.n_next
+                self.ptr.n_next.n_prev = self.ptr.n_prev
+
+
+    def dequeueCat(self):
+        self.ptr = self.head
+        while(self.ptr.type != 'cat'):
+            self.next()
+            self.dequeue()
+
+    def dequeueDog(self):
+        self.ptr = self.head
+        while(self.ptr.type != 'dog'):
+            self.next()
+            self.dequeue()
+
+    def dequeueAny(self):
+        self.ptr = self.head
+        self.dequeue()
+
+    #helper for linkedlist creation
+    #list of nodes
+def populate_shelter(list):
+    linkedlist = LinkedList()
+    for pet in list:
+      linkedlist.enqueue(pet['name'], pet['type'])
+    return linkedlist
 
 
 
@@ -208,6 +365,33 @@ def main():
     print("Dequeue:", end='')
     queue.removeQ()
     queue.printQ()
+
+    question3_5()
+    stack3_5 = sortStack([6,4,3])
+    print("Initialized Sort Stack: ", end='')
+    stack3_5.printSortStack()
+    stack3_5.sortPush(5)
+    print("Pushed 5 into Sort Stack: ", end='')
+    stack3_5.printSortStack()
+
+    question3_6()
+    shelter = populate_shelter([{'name': 'Gizmo', 'type': 'dog'}, {'name': 'Lucy', 'type': 'cat'}, {'name': 'Pebbles', 'type': 'dog'}])
+    print('Populated New Shelter: ', end='')
+    shelter.print_linked_list()
+    print('Enqueue new cat, Puurl:', end='')
+    shelter.enqueue('Puurl', 'cat')
+    shelter.print_linked_list()
+    print('Adopting ANY (dequeueAny)', end='')
+    shelter.dequeueAny()
+    shelter.print_linked_list()
+    print('Adopting DOG (dequeueDog)', end='')
+    shelter.dequeueDog()
+    shelter.print_linked_list()
+
+
+
+
+
 
 
 main()
